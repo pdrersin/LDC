0199f89b86fc4926b913f59d98abb9ce4947a885
Joe Derlaga <joseph.derlaga@gmail.com>
2011-12-20 16:30:56 -0500

    to make a quick 3x3 matrix inverse routine for testint

--- functions.f90
+++ functions.f90
@@ -88,6 +88,27 @@ module functions
 
     end function set_dt
 
+!================================= det_3x3 ===================================80
+!
+! computes determinant of a 3x3 matrix
+!
+!=============================================================================80
+
+    pure function det_3x3( mat )
+
+      use set_precision,  only : dp
+
+      real(dp), dimension(3,3), intent(in) :: mat
+      real(dp)                             :: det_3x3
+
+      continue
+
+      det_3x3 = mat(1,1)*(mat(2,2)*mat(3,3)-mat(2,3)*mat(3,2)) &
+              - mat(1,2)*(mat(2,1)*mat(3,3)-mat(2,3)*mat(3,1)) &
+              + mat(1,3)*(mat(2,1)*mat(3,2)-mat(2,2)*mat(3,1))
+
+    end function det_3x3
+
 !=========================== find_available_unit =============================80
 !
 ! finds an available I/O unit
--- ludcmp.f90
+++ ludcmp.f90
@@ -156,3 +156,40 @@ subroutine matrix_inv(neq, matrix, inv)
   end do
 
 end subroutine matrix_inv
+
+!=============================== mat_inv_3x3 =================================80
+!
+! Calculates inverse of 3x3 matrix for speed!!!!
+!
+!=============================================================================80
+
+subroutine mat_inv_3x3(mat, inv)
+
+  use set_precision, only : dp
+  use functions,     only : det_3x3
+
+  implicit none
+
+  real(dp), dimension(3,3), intent(in)  :: mat
+  real(dp), dimension(3,3), intent(out) :: inv
+
+  continue
+
+  inv(1,1) = mat(2,2)*mat(3,3)-mat(2,3)*mat(3,2)
+  inv(1,2) = mat(1,3)*mat(3,2)-mat(1,2)*mat(3,3)
+  inv(1,3) = mat(1,2)*mat(2,3)-mat(1,3)*mat(2,2)
+
+  inv(2,1) = mat(2,3)*mat(3,1)-mat(2,1)*mat(3,3)
+  inv(2,2) = mat(1,1)*mat(3,3)-mat(1,3)*mat(3,1)
+  inv(2,3) = mat(1,3)*mat(2,1)-mat(1,1)*mat(2,3)
+
+  inv(3,1) = mat(2,1)*mat(3,2)-mat(2,2)*mat(3,1)
+  inv(3,2) = mat(1,2)*mat(3,1)-mat(1,1)*mat(3,2)
+  inv(3,3) = mat(1,1)*mat(2,2)-mat(1,2)*mat(2,1)
+
+  inv = inv/det_3x3(mat)
+
+end subroutine mat_inv_3x3
+
+
+
