98cde3ab3f3b32357319488150e5a5be2473e706
Joe Derlaga <joseph.derlaga@gmail.com>
2012-01-23 13:38:13 -0500

    to use faster block tridiagonal solver

--- triblocksolve.f90
+++ triblocksolve.f90
@@ -1,41 +1,51 @@
-subroutine triblocksolve(neq, dof, L, D, U, RHS, x)
+!============================== triblocksolve ================================80
+!
+! Uses the Thomas algorithm to solve a generic block tridiagonal system
+! Destroys the DD, UD, RHS matrices
+!
+!=============================================================================80
+subroutine triblocksolve(neq, dof, LD, DD, UD, RHS, soln)
 
   use set_precision, only : dp
 
   implicit none
 
   integer,                          intent(in)    :: neq, dof
-  real(dp), dimension(neq,neq,dof), intent(inout) :: L, D, U
+  real(dp), dimension(neq,neq,dof), intent(inout) :: LD, DD, UD
   real(dp), dimension(neq,dof),     intent(inout) :: RHS
-  real(dp), dimension(neq,dof),     intent(out)   :: x
+  real(dp), dimension(neq,dof),     intent(out)   :: soln
 
-  integer :: i
-
-  real(dp), dimension(neq,dof) :: y
-  real(dp), dimension(neq,neq) :: A, AT, AT_inv, temp
+  integer                      :: i
+  real(dp), dimension(neq,neq) :: temp
 
   continue
 
-  A(:,:) = D(:,:,1)
-  y(:,1) = RHS(:,1)
+! Normalize the first row...
+!  call matrix_inv(neq, DD(:,:,1), temp)
+  call mat_inv_3x3(DD(:,:,1), temp)
 
+  DD(:,:,1) = matmul(temp,DD(:,:,1))
+  UD(:,:,1) = matmul(temp,UD(:,:,1))
+  RHS(:,1)  = matmul(temp,RHS(:,1))
+
+! Loop to eliminate lower diagonal and then normalize the row
   do i = 2, dof
-    AT = transpose(A)
-    call matrix_inv(neq, AT, AT_inv)
+    DD(:,:,i) = DD(:,:,i) - matmul(LD(:,:,i), UD(:,:,i-1))
+    RHS(:,i)  = RHS(:,i)  - matmul(LD(:,:,i), RHS(:,i-1))
 
-    D(:,:,i-1) = transpose(AT_inv)
-    temp = transpose( matmul( AT_inv, transpose(L(:,:,i)) ) ) 
+!    call matrix_inv(neq, DD(:,:,i), temp)
+    call mat_inv_3x3(DD(:,:,i), temp)
 
-    A      = D(:,:,i) - matmul(temp, U(:,:,i-1))
-    y(:,i) = RHS(:,i) - matmul(temp, y(:,i-1))
+    DD(:,:,i) = matmul(temp,DD(:,:,i))
+    UD(:,:,i) = matmul(temp,UD(:,:,i))
+    RHS(:,i)  = matmul(temp,RHS(:,i))
   end do
 
-  call matrix_inv(neq, A, AT_inv)
-
-  x(:,dof) = matmul(AT_inv, y(:,dof))
+! Back solve... since the diagonal is an identity matrix this is easy
+  soln(:,dof) = RHS(:,dof)
 
   do i = dof-1,1,-1
-    x(:,i) = matmul( D(:,:,i), (y(:,i) - matmul( U(:,:,i), x(:,i+1) )) )
+    soln(:,i) = RHS(:,i) - matmul(UD(:,:,i), soln(:,i+1))
   end do
 
 end subroutine triblocksolve
